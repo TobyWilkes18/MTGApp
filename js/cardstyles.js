79efb11cf60387bd109350204774a3c7d4c86e7c
Toby Wilkes <104756420+TobyWilkes18@users.noreply.github.com>
2022-09-07 18:07:49 -0400

    Add files via upload

---
+++ js/cardstyles.js
@@ -0,0 +1,39 @@
+/*
+**
+** A pen by Alex Buffet
+** https://alexandrebuffet.fr
+**
+**
+*/
+
+;(function ( $ ) {
+    $.fn.tilt = function() {
+        var perspective = '1000px',
+        delta = 20,
+        width = this.width(),
+        height = this.height(),
+        midWidth = width / 2,
+        midHeight = height / 2;
+        //Events
+        this.on({
+            mousemove: function(e) {
+                var pos = $(this).offset(),
+                cursPosX = e.pageX - pos.left,
+                cursPosY = e.pageY - pos.top,
+                cursCenterX = midWidth - cursPosX,
+                cursCenterY = midHeight - cursPosY;
+  
+                $(this).css('transform','perspective(' + perspective + ') rotateX('+ (cursCenterY / delta) +'deg) rotateY('+ -(cursCenterX / delta) +'deg)');
+                $(this).removeClass('is-out');
+            },
+            mouseleave: function() {
+                $(this).addClass('is-out');
+            }
+        });
+        //Return
+        return this;
+    };
+  }( jQuery ));
+  
+  //Set plugin on cards
+  $('.card').tilt();
