12fb98b42c1ff24fd467e259f93759c8044cff58
Toby Wilkes <104756420+TobyWilkes18@users.noreply.github.com>
2022-09-09 19:57:47 -0400

    Add files via upload

--- js/cardSearch.js
+++ js/cardSearch.js
@@ -33,6 +33,45 @@ function searchCard(url, methodType, callback){
         var cardImg = respJson.data[0].image_uris.normal;
 
         console.log(manaCost);
+
+        const manaArray = manaCost.split("");
+        manaArray.join();
+
+        console.log(manaArray);
+        //console.log(manaArray[0]);
+        //console.log(manaArray[1]);
+        //console.log(manaArray[2]);
+        //console.log("---");
+        //console.log(manaArray.length);
+
+        let i = 0;
+        let l = 0;
+        let arrPos = 0;
+        var symbolArray = [];
+
+        for(i; i<manaArray.length; i++){
+            //console.log(i+" - i number")
+            if (manaArray[i] == "{") {
+                arrPos = i;
+                //console.log(arrPos+" - arrPos");
+            }
+            if (manaArray[i] == "}") {
+                console.log(l+" - l iteration");
+                symbolArray[l] = manaArray.slice(arrPos,(i+1));
+                l+=1;
+            }
+        }
+
+        //for(let m=0; m<symbolArray.length; m++){
+        //    console.log(symbolArray[m]);
+        //    console.log("---");
+        //}
+        console.log(symbolArray);
+
+        //searchCard(searchURL, "GET", function(respJson){
+        //    
+        //});
+
         
         document.getElementById("cardName").innerHTML = cardName;
         document.getElementById("manaCost").innerHTML = manaCost;
